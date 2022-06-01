ff2dfeeec8baf1a029d5c9d19c6df062ba814efe
Jay Mathis <mathiscode@users.noreply.github.com>
2022-06-01 10:27:49 -0500

    loader reinit on hmr

--- src/_loader.js
+++ src/_loader.js
@@ -1,16 +1,10 @@
 import 'regenerator-runtime/runtime'
 
 import('./index').then(kernel => {
-  // Have to do a full reboot to get the new kernel during HMR
-  if (!globalThis.Kernel) globalThis.Kernel = kernel
+  globalThis.Kernel = kernel
 
   import('./terminal').then(term => {
-    let coldBoot = true
-
-    if (globalThis.Terminal) {
-      coldBoot = false
-      document.querySelector('#terminal').innerHTML = ''
-    }
+    if (globalThis.Terminal) document.querySelector('#terminal').innerHTML = ''
 
     globalThis.Terminal = term.create({
       fontFamily: "'Fira Mono', monospace",
@@ -23,12 +17,7 @@ import('./index').then(kernel => {
     globalThis.Terminal.fit()
     globalThis.Terminal.focus()
 
-    if (coldBoot) kernel.boot()
-    else {
-      globalThis.Terminal.log('System terminal reloaded via HMR')
-      globalThis.Terminal.prompt()
-
-    }
+    kernel.boot()
   })
 })
 
